c5e33ad2a44ea734b3ec1d781a6b5b270c0ca6ad
Ailyn Diaz <aidad@uc.cl>
2023-05-29 18:28:08 -0400

    cambios

--- src/main.js
+++ src/main.js
@@ -3,4 +3,13 @@ import { example } from './data.js';
 import data from './data/ghibli/ghibli.js';
 // import data from './data/rickandmorty/rickandmorty.js';
 
+const titles = ["Castle in the Sky", "Grave of the Fireflies", "Only Yesterday", "Porco Rosso", "Spirited Away"];
+
+// Filtrar las frutas que comienzan con la letra "a"
+const filteredFilms = titles.filter(title => title.charAt(0) === "a").sort();
+
+console.log(filteredFilms); // Resultado: ["Castle in the Sky"]
+
+alert('hola');
+
 console.log(example, data);
