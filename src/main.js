ef698f8edb0d818c5d39585a1e3c37320f29a6ac
natis93 <natis93_2011@hotmail.com>
2023-05-30 18:43:05 -0500

    pruebas listado

--- src/main.js
+++ src/main.js
@@ -10,9 +10,9 @@ const filteredFilms = titles.filter(title => title.charAt(0) === "a").sort();
 
 console.log(filteredFilms); // Resultado: ["Castle in the Sky"]*/
 
-const filmFilters = document.getElementById('film-filters');
-const filmList = document.getElementById('film-list');
-const films = filmList.querySelectorAll('.film');
+//const filmFilters = document.getElementById('film-filters');
+//const filmList = document.getElementById('film-list');
+//const films = filmList.querySelectorAll('.film');
 
 /*filmFilters.addEventListener('change', function() {
   const selectedValue = this.value;
@@ -45,9 +45,45 @@ const films = filmList.querySelectorAll('.film');
       });
   }
 });*/
-const filmsGhibli=dataGhibli.films
-for (let index = 0; index < filmsGhibli.length; index++) {
-  const element = filmsGhibli[index];
-console.log(element.title);    
-}
+//const filmsGhibli = dataGhibli.films
+//for (let index = 0; index < filmsGhibli.length; index++) {
+// const element = filmsGhibli[index];
+//console.log(element.title);}
 
+
+// Aquí iría el código JavaScript que muestra los títulos de las películas
+const filmsGhibli = dataGhibli.films;
+const filmList = document.getElementById('film-list');
+
+filmsGhibli.forEach(function (element) {
+  const filmTitle = document.createElement('p');
+  filmTitle.textContent = element.title;
+  filmList.appendChild(filmTitle);
+});
+
+// Supongamos que tienes un array de películas llamado "peliculas"
+const peliculas = [
+  { titulo: 'Película 1', director: 'Director 1' },
+  { titulo: 'Película 2', director: 'Director 2' },
+  { titulo: 'Película 3', director: 'Director 3' }
+];
+
+peliculas.forEach(function(pelicula) {
+  // Crear elemento de película
+  const peliculaElement = document.createElement('div');
+  peliculaElement.classList.add('pelicula');
+
+  // Crear elementos de título y director
+  const tituloElement = document.createElement('h3');
+  tituloElement.textContent = pelicula.titulo;
+
+  const directorElement = document.createElement('p');
+  directorElement.textContent = 'Director: ' + pelicula.director;
+
+  // Agregar elementos al contenedor de la película
+  peliculaElement.appendChild(tituloElement);
+  peliculaElement.appendChild(directorElement);
+
+  // Agregar la película al contenedor principal
+  filmList.appendChild(peliculaElement);
+});
